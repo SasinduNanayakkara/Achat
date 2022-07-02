fbbe89b531f222fc8a50b142fd2ddaab919e3851
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-03 03:04:25 +0530

    Define new API routes for get contacts

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -2,3 +2,4 @@ const host = 'http://localhost:5000';
 export const registerRoute = `${host}/api/register`;
 export const loginRoute = `${host}/api/login`;
 export const setAvatarRoute = `${host}/api/avatar`;
+export const allUsersRoute = `${host}/api/users`;
