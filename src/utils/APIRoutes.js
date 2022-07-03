79419fb0ce5a3f298394e60ccd02a7993c245629
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 02:34:48 +0530

    Define new routes

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -3,3 +3,5 @@ export const registerRoute = `${host}/api/register`;
 export const loginRoute = `${host}/api/login`;
 export const setAvatarRoute = `${host}/api/avatar`;
 export const allUsersRoute = `${host}/api/users`;
+export const sendMessageRoute = `${host}/api/msg/addmsg`;
+export const getMessagesRoute = `${host}/api/msg/getmsg`;
