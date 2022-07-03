63529deac042e5e31f7d507ab48214f2beaf8933
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 02:33:36 +0530

    Add new features to the chat page

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,15 +1,18 @@
-import React, {userState, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import "./index.css"
 import {useNavigate} from "react-router-dom"
 import axios from 'axios';
 import {allUsersRoute} from "../utils/APIRoutes";
 import Contact from "../components/Contact";
+import Welcome from '../components/Welcome';
+import ChatContainer from '../components/ChatContainer';
 const Chat = () => {
 
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
-
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     async function navigateTo()  {
       if (!localStorage.getItem("user")) {
@@ -17,34 +20,55 @@ const Chat = () => {
       }
       else {
         setCurrentUser(JSON.parse(localStorage.getItem("user")));
+        setIsLoaded(true);
       }
   }
   navigateTo();
-  })
-
+  },[navigate]);
+  
   useEffect(() => {
-    async function getContacts() { 
-      if (currentUser) {
+    const fetchData = async () => {
+      if(currentUser) {
         if (currentUser.isImageSet) {
-          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
-          setContacts(data.data);
+          const res = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          console.log("yes");
+          console.log(res.data);
+          setContacts(res.data);
         }
         else {
           navigate("/avatar");
         }
-        }
       }
-    getContacts();
-  }, [currentUser]);
+    }
+    fetchData();
+  }, [currentUser, navigate]);
+
+  console.log(currentUser);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  }
+
   return (
     <>
       <div className='h-screen w-full flex flex-col justify-center gap-4 items-center bg-gray-700'>
         <div className='h-4/5 w-4/5 bg-slate-600 grid grid-cols-2 container'>
-          <Contact contacts={contacts} currentUser={{currentUser}}/>
+          <Contact 
+            contacts={contacts} 
+            currentUser={currentUser} 
+            changeChat={handleChatChange}
+          />
+          {
+            isLoaded && currentChat === undefined ? (
+              <Welcome />
+            ) : (
+              <ChatContainer currentChat={currentChat} currentUser={currentUser}/>
+            )
+          }
         </div>
       </div>
     </>
   )
 }
 
-export default Chat
+export default Chat;
