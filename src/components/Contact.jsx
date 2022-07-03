eca048511e9ec1a3701a4fb2fd968a428b0070a6
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 02:32:12 +0530

    Chnage styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import "../pages/index.css"
-const Contact = ({contacts, currentUser}) => {
+import logo from "../asserts/chatX.png"
+
+const Contact = ({contacts, currentUser, changeChat}) => {
 
     const [currentUSername, setCurrentUSername] = useState(undefined);
     const [currentUserImage, setCurrentUserImage] = useState(undefined);
@@ -9,47 +11,56 @@ const Contact = ({contacts, currentUser}) => {
     useEffect(() => {
         if (currentUser) {
             setCurrentUSername(currentUser.username);
-            setCurrentUserImage(currentUser.image);
+            setCurrentUserImage(currentUser.avatarImage);
         }
     }, [currentUser]);
 
     const changeCurrentChat = (index, contact) => {
-
+        setCurrentUserSelected(index);
+        changeChat(contact);
     }
+    console.log(currentUSername);
+    console.log(currentUserImage);
+
   return (
     <>
         {
             currentUserImage && currentUSername && (
                 <div className='grid overflow-hidden bg-slate-600 chatContainer'>
                     <div className='flex items-center justify-center gap-4'>
-                        <img src="" alt="logo" className='h-8' />
-                        <h3 className='text-white'>Achat</h3>
+                        <img src={logo} alt="logo" className='h-11' />
+                        <h3 className='text-amber-300 text-4xl font-bold'>ChatX</h3>
                     </div>
-                    <div className="flex flex-col items-center overflow-auto gap-3 bg-slate-600">
+                    <div className="flex flex-col items-center mt-4 overflow-auto gap-3 bg-slate-600 contacts">
                         {
                             contacts.map((contact, index) => {
                                 return (
-                                    <div className={`contact 
-                                        ${index === currentUSerSelected ? "selected": ""}`}
+                                    <div className={`bg-slate-700 min-h-20 w-4/5 cursor-pointer rounded-sm p-2 flex items-center transition-all ease-in-out
+                                        ${index === currentUSerSelected ? "bg-slate-600": ""}`}
                                         key={index}
+                                        onClick={() => changeCurrentChat(index, contact)}
                                     >
                                         <div className="avatar">
                                             <img src={`data:image/svg+xml;base64,${contact.avatarImage}`} 
-                                            alt="avatar" />
+                                                alt="avatar"
+                                                className='h-12'
+                                            />
                                         </div>
-                                        <div className="username">
+                                        <div className="text-white px-5 text-lg">
                                             <h3>{contact.username}</h3>
                                         </div>
                                     </div>
                                 )
                             })}
                     </div>
-                    <div className="currentUser">
+                    <div className="bg-slate-700 flex justify-center mb-2 ml-2 items-center gap-4">
                         <div className="avatar">
                             <img src={`data:image/svg+xml;base64,${currentUserImage}`} 
-                                alt="avatar" />                                </div>
+                                alt="avatar"
+                                className='h-16'
+                                />                                </div>
                         <div className="username">
-                            <h2>{currentUSername}</h2>
+                            <h2 className='text-amber-400 text-lg'>{currentUSername}</h2>
                         </div>
                     </div>
                 </div>
