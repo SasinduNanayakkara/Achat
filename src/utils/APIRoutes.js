ab185c7f97ce45a5bc1d9e37a574f6be01379bfc
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 21:50:59 +0530

    Export the host

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-const host = 'http://localhost:5000';
+export const host = 'http://localhost:5000';
 export const registerRoute = `${host}/api/register`;
 export const loginRoute = `${host}/api/login`;
 export const setAvatarRoute = `${host}/api/avatar`;
