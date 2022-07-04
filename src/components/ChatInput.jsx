2aaa01165e756ec7ba68a304985fc02ef8bf4240
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 16:06:01 +0530

    Chnage styles

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -27,21 +27,21 @@ const ChatInput = ({handleSendMsg}) => {
     }
 
   return (
-    <div className='grid items-center mb-20 bg-slate-700 px-1 pb-2 msgInputContainer rounded-full'>
-        <div className="flex items-center px-2 pt-1 text-white gap-4">
-            <div className="relative ">
-            <BsEmojiSmileFill onClick={handleEmojiPickerHideShow} className='text-xl text-amber-400 cursor-pointer'/>
+    <div className=' grid items-center bg-slate-700 mx-5 px-1 msgInputContainer rounded-full'>
+        <div className="relative flex items-center px-2 pt-1 text-white gap-4">
+            <div className="relative">
+            <BsEmojiSmileFill onClick={handleEmojiPickerHideShow} className='text-3xl text-amber-400 cursor-pointer'/>
             {emojiPicker &&  <Picker className="absolute -top-96 bg-slate-600 shadow-lg border-slate-600" onEmojiClick={handleEmojiClick}/>}
             </div>
         </div>
     <form onSubmit={(e) => sendChat(e)} className='w-full rounded-md flex content-center gap-8 bg-slate-700'>
-        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='w-5/6 h-3/5 text-white bg-transparent border-none pl-4 text-lg selection:bg-slate-400 focus:outline-none' placeholder='Type Message' id="" />
-        <button type="submit" className='py-2 px-8 rounded-full gap-4 flex justify-center items-center bg-amber-500 border-none'>
-            <IoMdSend className='text-lg text-black'/>
+        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} className='w-5/6 h-3/5 pt-2 text-white bg-transparent border-none pl-6 text-lg selection:bg-slate-400 focus:outline-none' placeholder='Type Message' id="" />
+        <button type="submit" className='py-3 px-7 rounded-full gap-4 flex justify-center items-center bg-amber-500 border-none'>
+            <IoMdSend className='text-xl text-black'/>
         </button>
     </form>
     </div>
   )
 }
 
-export default ChatInput
+export default ChatInput;
