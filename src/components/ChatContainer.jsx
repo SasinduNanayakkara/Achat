3d982aa12adbd2ef635f8a0e7f6b3f99d112b5e4
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-04 16:05:44 +0530

    Implement load mesages feature

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import ChatInput from './ChatInput';
 import Logout from './Logout';
 import Messages from './Messages';
-import { sendMessageRoute } from '../utils/APIRoutes';
+import "../pages/index.css"
+import { getMessagesRoute, sendMessageRoute } from '../utils/APIRoutes';
 
 const ChatContainer =  ({currentChat, currentUser}) =>  {
+
+    const [messages, setMEssages] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.post(getMessagesRoute, {
+                from: currentUser._id,
+                to: currentChat._id,
+            });
+            setMEssages(res.data);
+            console.log("messageData " + res.data);
+            console.log("currentUser " + currentUser._id);
+            console.log("currentChat " + currentChat._id);
+        }
+        fetchData();
+    }, [currentChat, currentUser]);
+
+
 
     const handleSendMsg = async (msg) => {
         await axios.post(sendMessageRoute, {
@@ -18,7 +37,7 @@ const ChatContainer =  ({currentChat, currentUser}) =>  {
     <>
     {
         currentChat && (
-    <div className='p-4'>
+    <div className='pt-4 pb-0 chatContentContainer'>
         <div className="flex justify-between items-center py-0 px-8">
             <div className="flex items-center gap-4 ">
                 <div className="avatar">
@@ -33,8 +52,22 @@ const ChatContainer =  ({currentChat, currentUser}) =>  {
             </div>
             <Logout/>
         </div>
-        <div className="chatMessages">
-            
+        <div className="py-2 px-8 flex flex-col gap-2 overflow-auto">
+            {
+                messages.map(messages => {
+                    return (
+                        <div>
+                            <div className={`flex items-center ${messages.fromSelf ? "justify-end bg-slate-400 send": "justify-start bg-gray-500 received"}`}>
+                                <div className="max-w-sm break-words p-4 text-base rounded-lg text-white content">
+                                    <p>
+                                        {messages.message}
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                    );
+                })}
         </div>
         <ChatInput handleSendMsg={handleSendMsg}/>
     </div>
